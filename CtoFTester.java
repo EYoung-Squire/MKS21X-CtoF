25ca19221098a46d2b9c1b5cb1dc2072b0fe4403
EYoung-Squire <78228654+EYoung-Squire@users.noreply.github.com>
2021-09-23 16:22:17 -0400

    Add files via upload

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+public class CtoFTester {
+
+  //The parameter should be celcius
+  //The type should be double
+  //The function should return double farenheit
+  public static double celciusToFarenheit(double celcius) {
+    double farenheit = (celcius * (9/5)) + 32;
+    return farenheit;
+  }
+
+}
